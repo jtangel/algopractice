d07a3a330d917dd6160594abceee111206f788c5
Jack Tangel <jackmtangel@gmail.com>
2022-10-01 11:26:12 -0500

    more algos

--- hacker_rank_worksheet.py
+++ hacker_rank_worksheet.py
@@ -1,5 +1,5 @@
 
-# Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+# # Given a square matrix, calculate the absolute difference between the sums of its diagonals.
 
 def diagonalDifference(arr):
     sum1 = 0
@@ -13,7 +13,7 @@ def diagonalDifference(arr):
     return abs(sum1 - sum2)
 
 
-# print(diagonalDifference(((11,2,4), (4,5,6), (10,8,-12))))
+print(diagonalDifference(((11,2,4), (4,5,6), (10,8,-12))))
 
 
 
@@ -33,12 +33,12 @@ def plusMinus(arr):
     print(format(zer,'.6f'))
 
 
-# plusMinus((-4, 3, -9, 0, 4, 1))
+plusMinus((-4, 3, -9, 0, 4, 1))
 
 
-# Its base and height are both equal to n It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+# # # Its base and height are both equal to n It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
 
-# Write a program that prints a staircase of n size .
+# # # Write a program that prints a staircase of n size .
 
 def staircase(n):
     stairs = ''
@@ -48,3 +48,47 @@ def staircase(n):
         
 
 staircase(6)
+
+# # Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+
+
+def miniMaxSum(arr):
+    maxi = 0
+    mini = 0
+    x = min(arr)
+    y = max(arr) 
+    for i in range(len(arr) - 1):
+        if arr[i] == x and arr[i] == y:
+            maxi += arr[i]
+            mini += arr[i]
+    for i in range(len(arr)):
+        if arr[i] != x:
+            maxi += arr[i]
+        if arr[i] != y:
+            mini += arr[i]
+    print(mini, maxi)
+
+
+miniMaxSum([5,5,5,5,5])
+miniMaxSum([1,2,3,4,5])
+
+
+# You are in charge of the cake for a child's birthday. You have decided the cake will have one candle for each year of their total age. They will only be able to blow out the tallest of the candles. Count how many candles are tallest.
+
+
+
+
+def birthdayCakeCandles(candles):
+    byeFire = 0
+    max_height = max(candles)
+    for i in range(len(candles)):
+        if candles[i] == max_height:
+            byeFire += 1
+    print(byeFire)
+    return byeFire
+
+
+birthdayCakeCandles([1,4,4,4,3,2])
+
+
