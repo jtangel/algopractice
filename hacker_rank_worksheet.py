2e6482d5880fec40a6764907f27a13cab1292481
Jack Tangel <jackmtangel@gmail.com>
2022-10-04 10:38:30 -0500

    more algo practice

--- hacker_rank_worksheet.py
+++ hacker_rank_worksheet.py
@@ -92,10 +92,10 @@ def birthdayCakeCandles(candles):
 birthdayCakeCandles([1,4,4,4,3,2])
 
 
-# Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+# # Given a time in -hour AM/PM format, convert it to military (24-hour) time.
 
-# Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
-# - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock
+# # Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+# # - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock
 
 
 
@@ -123,3 +123,22 @@ print(timeConversion('11:00:00PM'))
 print(timeConversion('12:45:54PM'))
 
 
+
+# HackerLand University has the following grading policy:
+
+# Every student receives a  in the inclusive range from 0 to 100.
+# Any grade less than  40 is a failing grade.
+# Sam is a professor at the university and likes to round each student's grade according to these rules:
+
+# If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+# If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+
+def gradingStudents(grades):
+    for i in range(len(grades)):
+        if grades[i] >= 38:
+            if (grades[i] + 2) % 5 == 0 or (grades[i] + 1) % 5 == 0:
+                grades[i] = (round(grades[i]/5.0) * 5)
+    return grades
+
+
+print(gradingStudents([73, 67, 38, 33]))
