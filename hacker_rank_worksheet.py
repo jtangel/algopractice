1d5bcb2ecce104fa9079048bf674c778e63ecaea
Jack Tangel <jackmtangel@gmail.com>
2022-10-05 10:45:12 -0500

    more practice

--- hacker_rank_worksheet.py
+++ hacker_rank_worksheet.py
@@ -142,3 +142,53 @@ def gradingStudents(grades):
 
 
 print(gradingStudents([73, 67, 38, 33]))
+
+
+# Sam's house has an apple tree and an orange tree that yield an abundance of fruit. Using the information given below, determine the number of apples and oranges that land on Sam's house.
+
+#  info on hackerank website
+
+# s: integer, starting point of Sam's house location.
+# t: integer, ending location of Sam's house location.
+# a: integer, location of the Apple tree.
+# b: integer, location of the Orange tree.
+# apples: integer array, distances at which each apple falls from the tree.
+# oranges: integer array, distances at which each orange falls from the tree.
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    applesForSam = 0
+    orangesForSam = 0
+    for i in apples:
+        if (a + i) in range(s, t+1):
+            applesForSam += 1
+    for x in oranges:
+        if (b+x) in range(s, t+1):
+            orangesForSam += 1
+    print(applesForSam)
+    print(orangesForSam)
+        
+
+# You are choreographing a circus show with various animals. For one act, you are given two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity).
+
+# The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump.
+# The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
+# You have to figure out a way to get both kangaroos at the same location at the same time as part of the show. If it is possible, return YES, otherwise return NO.
+
+def kangaroo(x1, v1, x2, v2):
+    jumps = []
+    isPossible = False
+    for i in range(0,10001):
+        x1 += v1
+        jumps.append(x1)
+        x2 += v2
+        jumps.append(x2)
+    print(jumps)
+    for x in range(len(jumps) - 1):
+        if jumps[x] == jumps[x +1]:
+            isPossible = True
+    if isPossible == True:
+        return 'YES'
+    else: return 'NO'
+
+
+
